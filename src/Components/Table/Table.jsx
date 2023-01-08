21411382f4b92d16fc6b580af85f619c40b5bb26
umidkhan <umidxondeveloper@mail.ru>
2023-01-08 17:20:03 +0500

    table fix

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -17,34 +17,32 @@ export default function Table() {
   }, []);
   return (
     <>
-      <div className="table">
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Website</th>
-            <th>Company</th>
-          </tr>
-          {data.map((item) => {
-            return (
-              <tr>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
-                <td>{item.address.street}</td>
-                <td>{item.phone}</td>
-                <td>{item.website}</td>
-                <td>{item.company.name}</td>
-              </tr>
-            );
-          })}
-        </table>
-      </div>
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Website</th>
+          <th>Company</th>
+        </thead>
+        {data.map((item) => {
+          return (
+            <tbody>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td>{item.address.street}</td>
+              <td>{item.phone}</td>
+              <td>{item.website}</td>
+              <td>{item.company.name}</td>
+            </tbody>
+          );
+        })}
+      </table>
     </>
   );
 }
